9c43c161e91a2eea57fb90925785839cba722db9
Rebecca Skinner <rebecca@rebeccaskinner.net>
2016-07-16 15:20:32 -0500

    updates

--- src/Main.hs
+++ src/Main.hs
@@ -5,10 +5,11 @@ import Network.Wai.Middleware.Static
 import System.Environment (getArgs)
 import System.IO
 import Data.Maybe
+import Data.Char
 import Control.Monad.IO.Class
-import Database.HDBC
 import Database.HDBC.Sqlite3
 import Site.Post
+import Network.HTTP.Types
 
 getDBArg :: IO String
 getDBArg = do
@@ -29,16 +30,32 @@ getDBArg = do
 
 sqliteConnection = getDBArg >>= connectSqlite3
 
+includePostContent :: String -> Bool
+includePostContent s = if "true" == (map toLower s) then True else False
+
 main :: IO ()
 main =
   scotty 3000 $ do
-
   middleware $ staticPolicy (noDots >-> addBase "static")
 
   get "/" $ file "./static/index.html"
+
   get "/posts" $ do
-    posts <- liftIO $ (sqliteConnection >>= getPosts)
-    json posts
+    includeContents <- (param "include-contents") `rescue` (\_ -> return "false")
+    posts <- liftIO $ sqliteConnection >>= getPosts
+    if includePostContent includeContents then
+      json posts
+      else
+      json $ map postID posts
+
+  get "/posts/:post" $ do
+    post <- param "post"
+    p <- liftIO $ sqliteConnection >>= flip postByID post
+    case p of
+      Just aPost -> json p
+      Nothing -> file "./static/error.html" >>
+                 addHeader "Content-Type" "text/html" >>
+                 status status404
 
   get "/:word" $ do
     beam <- param "word"
